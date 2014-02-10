aa5c714428038a12ee616c65c803212d66e4307c
Yusuf Simonson <simonson@gmail.com>
2014-02-10 14:26:35 -0500

    Use phantom-cluster develop branch

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var prerender = require('./lib')
 
 var server = prerender({
     workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
-    iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
+    workerIterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
+    workerParallelism: process.env.PHANTOM_WORKER_PARALLELISM || 1,
     phantomArguments: ["--load-images=false", "--ignore-ssl-errors=true"],
     phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT,
     messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
